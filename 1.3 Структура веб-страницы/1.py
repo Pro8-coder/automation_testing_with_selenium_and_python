524ab84f625b1f6d2ca4914d0cc7b72b861a3f37
Pro8 <111078922+Pro8-coder@users.noreply.github.com>
2025-05-27 11:12:09 +0300

    1.3 1-2

--- 1.3 Структура веб-страницы/1.py
+++ 1.3 Структура веб-страницы/1.py
@@ -1,5 +1,24 @@
 """
+Заголовок синий
+Текст скрыт
+Кнопка заблокирована
 
+HTML
+<h1> Этот заголовок будет синим </h1>
+<p> Этот атрибут скрывает элемент на странице, элемент не будет показываться </p>
+<button> Кнопка с атрибутом disabled будет заблокирована </button>
+
+CSS
+button {
+  background-color: #6c6;
+  border: none;
+  color: white;
+  padding: 15px 30px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+}
 """
 
 
--- 1.3 Структура веб-страницы/2.py
+++ 1.3 Структура веб-страницы/2.py
@@ -1,5 +1,27 @@
 """
+Добавьте элемент-картинку с классом picture
+Добавьте элементу div атрибут name со значением about
+Добавьте элементу div потомка-заголовок с тегом h3 и потомка-текст с тегом p
+Добавьте элементу-заголовку h3 атрибут id со значением pet-name
+Добавьте элементу с текстом (<p>) атрибут data-type и значением description
 
+HTML
+<!DOCTYPE HTML>
+<html>
+ <body>
+  <h1>Look at my awesome pet:</h1>
+  <img src="">
+  <div>
+
+  </div>
+ </body>
+</html>
+
+CSS
+.picture  {
+    width:100%;
+    max-width:200px;
+}
 """
 
 
