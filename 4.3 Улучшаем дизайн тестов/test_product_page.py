467a199f927dc221d5c901e985f8a733f2a5ae38
Pro8 <111078922+Pro8-coder@users.noreply.github.com>
2025-06-23 20:43:25 +0300

    4.3 update(def test_guest_cant_see_success_message_after_adding_product_to_basket, def test_guest_cant_see_success_message, def test_message_disappeared_after_adding_product_to_basket)

--- 4.3 Улучшаем дизайн тестов/test_product_page.py
+++ 4.3 Улучшаем дизайн тестов/test_product_page.py
@@ -18,3 +18,33 @@ def test_guest_can_add_product_to_basket(browser, promo):
     page.name_validity_check()
     page.price_validity_check()
     page.should_disappeared()
+
+
+def test_guest_cant_see_success_message_after_adding_product_to_basket(
+        browser):
+    page = ProductPage(
+        browser,
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    )
+    page.open()
+    page.add_to_basket()
+    page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    page = ProductPage(
+        browser,
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    )
+    page.open()
+    page.should_not_be_success_message()
+
+
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    page = ProductPage(
+        browser,
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    )
+    page.open()
+    page.add_to_basket()
+    page.should_disappeared()
